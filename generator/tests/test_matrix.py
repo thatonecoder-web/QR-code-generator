6b1c1060ef5b4b876590e12fc9d2c490f9600759
awebdev <iman123ahmadi@outlook.com>
2026-09-13 16:54:13 -0400

    Added tests for QRMatrix class
    Added unit tests for QRMatrix functionality including default size, set/get methods, and initial state.

--- generator/tests/test_matrix.py
+++ generator/tests/test_matrix.py
@@ -1,1 +1,23 @@
+from qr_generator.matrix import QRMatrix
 
+
+def test_default_matrix_size():
+    matrix = QRMatrix()
+
+    assert matrix.size == 21
+    assert len(matrix.to_list()) == 21
+    assert len(matrix.to_list()[0]) == 21
+
+
+def test_matrix_set_and_get():
+    matrix = QRMatrix()
+
+    matrix.set(5, 5, True)
+
+    assert matrix.get(5, 5) is True
+
+
+def test_matrix_starts_empty():
+    matrix = QRMatrix()
+
+    assert matrix.get(0, 0) is None
